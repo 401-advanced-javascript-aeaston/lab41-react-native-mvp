b46a56ea7940b2634cc5a89990126ab1552ff841
Adrienne <adrienneeaston@gmail.com>
2019-10-23 21:40:36 -0700

    trying various ideas

--- app.js
+++ app.js
@@ -78,3 +78,30 @@ export default () => {
 //   } />
 
 // this.slideRight.start()
+
+
+
+
+
+// import React from 'react';
+// import { StyleSheet, Text, View, Button } from 'react-native';
+// import RandomStr from './component/randomString.js'
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Just shake!</Text>
+//       <RandomStr/>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
